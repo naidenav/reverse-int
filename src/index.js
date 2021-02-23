e31edf8285dce79100ee1931363d9cf2462749af
Alexander Naiden <naidenav@gmail.com>
2021-02-23 17:20:20 +0300

    init: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	n = n + '';
+	let result = [];
+	for(let i = n.length - 1; i >= 0; i--) {
+		if(n[i] != '-') {
+            result.push(n[i]);
+        }
+	};
+	result = result.join('');
+	return +result;
 }
